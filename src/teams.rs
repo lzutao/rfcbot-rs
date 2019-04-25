3b5674c1d0d358459a5b701b3d1c26dd0bbc8438
Lzu Tao <taolzu@gmail.com>
2019-04-25 20:10:51 +0700

    clippy: Fix redundant_closure

--- src/teams.rs
+++ src/teams.rs
@@ -104,7 +104,7 @@ impl Team {
     pub fn ping(&self) -> &str { &self.ping }
 
     pub fn member_logins(&self) -> impl Iterator<Item = &str> {
-        self.members.iter().map(|s| s.as_str())
+        self.members.iter().map(std::string::String::as_str)
     }
 }
 
